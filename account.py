d2bc5ae51df4e51e58bd433c4c0369a2b5a6f1fa
jerry8tom <mukul02015@gmail.com>
2020-01-22 03:23:45 +1100

    add class and make it look OO

---
+++ account.py
@@ -0,0 +1,29 @@
+from abc import  ABC, abstractmethod
+from rest import restBalance
+
+class Account ():
+
+	def __init__ (self, pub_k, pvt_k):
+		self._pub_k = pub_k
+		self._pvt_k = pvt_k
+
+	@property
+	def pub_k (self):
+		return self._pub_k
+	@pub_k.setter
+	def pub_k (self, new_pub):
+		self._pub_k = new_pub
+
+	@property
+	def pvt_k (self):
+		return self._pvt_k
+	@pvt_k.setter
+	def pvt_k (self, new_pvt):
+		self._pvt_k = new_pvt
+
+	def balance (self):
+		return restBalance (pub_k (self), pvt_k (self))
+
+	def __str__ (self):
+		return f'Account info:\n public key-> {self._pub_k}\n private key-> {self._pvt_k}\n balance-> {balance(self)}'
+
